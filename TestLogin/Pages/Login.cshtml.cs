4af5cfc9c41afa6f763a31d1cdee8053e5ccdd31
Землянских Максим <gotfot24@gmail.com>
2020-11-25 10:50:25 +0600

    что-то сделал

---
+++ TestLogin/Pages/Login.cshtml.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Http;
+using TestLogin.Pages.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace TestLogin.Pages
+{
+    public class LoginModel : PageModel
+    {
+        public string Message = "ETO:";
+        public string LoginState = "";
+        private readonly TestLogin.Pages.Models.UserContext _context;
+        public bool isGood;
+
+        //public LoginModel(TestLogin.Pages.Models.UserContext context)
+        // {
+        //    _context = context;
+        //}
+        [BindProperty]
+        public IList<User> User { get; set; }
+        
+
+        public void OnPost(string log, string pwd)
+        {
+            foreach(var item in User)
+            {
+                    Message += item.Login;
+            }
+        }
+    }
+}
---
+++ TestLogin/Pages/Models/UserContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace TestLogin.Pages.Models
+{
+    public class UserContext : DbContext
+    {
+        public DbSet<User> User { get; set; }
+        public UserContext(DbContextOptions<UserContext> options)
+            : base(options)
+        {
+        }
+    }
+}
